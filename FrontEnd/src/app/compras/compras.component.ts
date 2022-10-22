30f7c7f1276daa9f2c0f53255d0de34abca7e1a8
LESilva <luis.silva.LJS@outlook.com>
2022-10-22 20:31:09 -0300

    Modificado layout de vendas e compras: -> Adicionado campos de pesquisa -> Campos que estavam como inativos trocados para readonly -> Titulo alterado 'FrontEnd' > 'SmartWorkspace'
    modified:   FrontEnd/src/app/compras/compras.component.html
    modified:   FrontEnd/src/app/compras/compras.component.ts
    modified:   FrontEnd/src/app/vendas/vendas.component.html
    modified:   FrontEnd/src/app/vendas/vendas.component.ts
    modified:   FrontEnd/src/index.html

--- FrontEnd/src/app/compras/compras.component.ts
+++ FrontEnd/src/app/compras/compras.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compras.component.css']
 })
 export class ComprasComponent implements OnInit {
+  
+  public itens = [
+    {id: '1', descricao: 'Primeiro', qtd: 2, precoUn: 150},
+    {id: '2', descricao: 'Segundo', qtd: 3, precoUn: 200},
+  ];
+
+  public qtdItens = this.itens.length;
+  public totalItens = 0;
 
   constructor() { }
 
--- FrontEnd/src/app/vendas/vendas.component.ts
+++ FrontEnd/src/app/vendas/vendas.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendas.component.css']
 })
 export class VendasComponent implements OnInit {
+  
+  public itens = [
+    {id: '1', descricao: 'Primeiro', qtd: 2, precoUn: 150},
+    {id: '2', descricao: 'Segundo', qtd: 3, precoUn: 200},
+  ];
 
+  public qtdItens = this.itens.length;
+  public totalItens = 0;
+  
   constructor() { }
 
   ngOnInit() {
